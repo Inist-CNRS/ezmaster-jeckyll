64cf00bb94fedf1f1943f6ab42d1237a30d262d8
Stéphane Gully <stephane.gully@gmail.com>
2018-06-14 22:10:26 +0200

    improve stability of build

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
-/watcher.sh &
+cd /srv/ezmaster-jekyll/
 
-chmod -R 777 /srv/ezmaster-jekyll/ezmaster-site/
+chmod -R 777 ./ezmaster-site/
 
-if [ ! -d /srv/ezmaster-jekyll/ezmaster-site/_posts/ ]; then
-  jekyll new --force ezmaster-site
-  
-  sed -i 's#^date:.*$#date: 2018-06-14#g' /srv/ezmaster-jekyll/ezmaster-site/_posts/0000-00-00-welcome-to-jekyll.markdown.erb
+if [ ! -d ./ezmaster-site/_posts/ ]; then
+  jekyll new --force ezmaster-site-init
+  cat ./ezmaster-site-init/_config.yml > ./ezmaster-site/_config.yml
+  cp -rf ./ezmaster-site-init/* ./ezmaster-site/
 
-  cp -rf $(bundle show minima | tail -n 1)/_includes/ /srv/ezmaster-jekyll/ezmaster-site/
-  cp -rf $(bundle show minima | tail -n 1)/_layouts/  /srv/ezmaster-jekyll/ezmaster-site/
-  cp -rf $(bundle show minima | tail -n 1)/_sass/     /srv/ezmaster-jekyll/ezmaster-site/
-  cp -rf $(bundle show minima | tail -n 1)/assets/    /srv/ezmaster-jekyll/ezmaster-site/
+  cp -rf $(bundle show minima | tail -n 1)/_includes/ ./ezmaster-site/
+  cp -rf $(bundle show minima | tail -n 1)/_layouts/  ./ezmaster-site/
+  cp -rf $(bundle show minima | tail -n 1)/_sass/     ./ezmaster-site/
+  cp -rf $(bundle show minima | tail -n 1)/assets/    ./ezmaster-site/
 
-  chmod -R 777 /srv/ezmaster-jekyll/ezmaster-site/
+  chmod -R 777 ./ezmaster-site/
 fi
 
+/watcher.sh &
 
-exec jekyll serve --skip-initial-build --source /srv/ezmaster-jekyll/ezmaster-site/
+exec jekyll serve --skip-initial-build --source ./ezmaster-site/
