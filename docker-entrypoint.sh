6dc13c9ac3406a3104a6afc2d090a4f5b9a8cac8
Stéphane Gully <stephane.gully@gmail.com>
2018-06-14 18:54:37 +0200

    copy the default minima theme directly into the first website

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,10 @@
 
 if [ ! -d /srv/ezmaster-jekyll/ezmaster-site/_posts/ ]; then
   jekyll new --force ezmaster-site
+  cp -rf $(bundle show minima)/_includes/ /srv/ezmaster-jekyll/ezmaster-site/
+  cp -rf $(bundle show minima)/_layouts/  /srv/ezmaster-jekyll/ezmaster-site/
+  cp -rf $(bundle show minima)/_sass/     /srv/ezmaster-jekyll/ezmaster-site/
+  cp -rf $(bundle show minima)/assets/    /srv/ezmaster-jekyll/ezmaster-site/
 fi
 
 chmod -R 777 /srv/ezmaster-jekyll/ezmaster-site/
