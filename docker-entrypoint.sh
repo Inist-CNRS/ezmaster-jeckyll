96bbcce445ed47f9a39301198555365c67889f4d
Stéphane Gully <stephane.gully@gmail.com>
2018-06-14 16:54:57 +0200

    correct 777 rights after creating the blank website

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,10 +2,10 @@
 
 /watcher.sh &
 
-chmod -R 777 /srv/ezmaster-jekyll/ezmaster-site/
-
 if [ ! -d /srv/ezmaster-jekyll/ezmaster-site/_posts/ ]; then
   jekyll new --force ezmaster-site
 fi
+
+chmod -R 777 /srv/ezmaster-jekyll/ezmaster-site/
 
 exec jekyll serve --skip-initial-build --source /srv/ezmaster-jekyll/ezmaster-site/
