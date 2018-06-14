7313fc0eea343988cb49f36997fd6c28f71e32f1
Stéphane Gully <stephane.gully@gmail.com>
2018-06-14 20:41:48 +0200

    need a first chown because of empty _config.yml coming from ezmaster docker volume (config file)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,8 @@
 
 /watcher.sh &
 
+chmod -R 777 /srv/ezmaster-jekyll/ezmaster-site/
+
 if [ ! -d /srv/ezmaster-jekyll/ezmaster-site/_posts/ ]; then
   jekyll new --force ezmaster-site
   
@@ -11,8 +13,9 @@ if [ ! -d /srv/ezmaster-jekyll/ezmaster-site/_posts/ ]; then
   cp -rf $(bundle show minima)/_layouts/  /srv/ezmaster-jekyll/ezmaster-site/
   cp -rf $(bundle show minima)/_sass/     /srv/ezmaster-jekyll/ezmaster-site/
   cp -rf $(bundle show minima)/assets/    /srv/ezmaster-jekyll/ezmaster-site/
+
+  chmod -R 777 /srv/ezmaster-jekyll/ezmaster-site/
 fi
 
-chmod -R 777 /srv/ezmaster-jekyll/ezmaster-site/
 
 exec jekyll serve --skip-initial-build --source /srv/ezmaster-jekyll/ezmaster-site/
