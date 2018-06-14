9414c4843a520afd39cb96ecb5a7c54c296aa8b4
Stéphane Gully <stephane.gully@gmail.com>
2018-06-14 16:44:46 +0200

    better initialization of blank website

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,8 +4,8 @@
 
 chmod -R 777 /srv/ezmaster-jekyll/ezmaster-site/
 
-if [ ! -f /srv/ezmaster-jekyll/ezmaster-site/_config.yml ]; then
-  jekyll new ezmaster-site
+if [ ! -d /srv/ezmaster-jekyll/ezmaster-site/_posts/ ]; then
+  jekyll new --force ezmaster-site
 fi
 
 exec jekyll serve --skip-initial-build --source /srv/ezmaster-jekyll/ezmaster-site/
