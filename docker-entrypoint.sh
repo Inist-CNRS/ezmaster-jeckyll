5e9996f81c1ea0cc75d47201e3602275650ec945
Stéphane Gully <stephane.gully@gmail.com>
2018-06-14 18:57:51 +0200

    patch the default date with a correct one so that jekyll is able to build without error

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,9 @@
 
 if [ ! -d /srv/ezmaster-jekyll/ezmaster-site/_posts/ ]; then
   jekyll new --force ezmaster-site
+  
+  sed -i 's#^date:.*$#date: 2018-06-14#g' /srv/ezmaster-jekyll/ezmaster-site/_posts/0000-00-00-welcome-to-jekyll.markdown.erb
+
   cp -rf $(bundle show minima)/_includes/ /srv/ezmaster-jekyll/ezmaster-site/
   cp -rf $(bundle show minima)/_layouts/  /srv/ezmaster-jekyll/ezmaster-site/
   cp -rf $(bundle show minima)/_sass/     /srv/ezmaster-jekyll/ezmaster-site/
