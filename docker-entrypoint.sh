ee3be7844c0a77c48e1b870b9411fa2b935f1ce3
Stéphane Gully <stephane.gully@gmail.com>
2018-06-14 21:36:29 +0200

    fix cp error

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,10 +9,10 @@ if [ ! -d /srv/ezmaster-jekyll/ezmaster-site/_posts/ ]; then
   
   sed -i 's#^date:.*$#date: 2018-06-14#g' /srv/ezmaster-jekyll/ezmaster-site/_posts/0000-00-00-welcome-to-jekyll.markdown.erb
 
-  cp -rf $(bundle show minima)/_includes/ /srv/ezmaster-jekyll/ezmaster-site/
-  cp -rf $(bundle show minima)/_layouts/  /srv/ezmaster-jekyll/ezmaster-site/
-  cp -rf $(bundle show minima)/_sass/     /srv/ezmaster-jekyll/ezmaster-site/
-  cp -rf $(bundle show minima)/assets/    /srv/ezmaster-jekyll/ezmaster-site/
+  cp -rf $(bundle show minima | tail -n 1)/_includes/ /srv/ezmaster-jekyll/ezmaster-site/
+  cp -rf $(bundle show minima | tail -n 1)/_layouts/  /srv/ezmaster-jekyll/ezmaster-site/
+  cp -rf $(bundle show minima | tail -n 1)/_sass/     /srv/ezmaster-jekyll/ezmaster-site/
+  cp -rf $(bundle show minima | tail -n 1)/assets/    /srv/ezmaster-jekyll/ezmaster-site/
 
   chmod -R 777 /srv/ezmaster-jekyll/ezmaster-site/
 fi
